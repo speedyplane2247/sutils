02eed6d397f562a120383ef2d642272a2908fef3
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-03-02 22:21:23 -0500

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,4 +1,23 @@
-showCredits()
-function showCredits() {
-    $alert.info("Sutils, a standard utility pack for Windows 93. (c) speedyplane2247 2017-2018")
-}
+function Sutils () {
+    this.version = function ()
+      {
+          $alert.info("Sutils 1.0")
+      }
+    this.x3Detect = function (pass) 
+    {
+        if ($boot.daemon == "noX3") {
+            var pass = "detected"
+            return pass
+        }
+         else 
+         {
+var pass = "notdetected"
+return pass
+        }
+    }
+    this.credits = function () {
+        $alert.info("Sutils 1.0 - Written by speedyplane2447 (c) speedyplane2247 2017-2018")
+    }
+  }
+  
+  var sutils = new Sutils ();
