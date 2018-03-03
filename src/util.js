89ea406f97271d5bf52bc354f748e58ce2727846
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-03-02 22:26:33 -0500

    Update util.js

--- src/util.js
+++ src/util.js
@@ -18,6 +18,17 @@ return pass
     this.credits = function () {
         $alert.info("Sutils 1.0 - Written by speedyplane2447 (c) speedyplane2247 2017-2018")
     }
+    this.checkFor93inject = function (pass) {
+        if ($boot.daemonArgs.indexOf("X3Substrate")) {
+var pass = "detected"
+return pass
+        }
+        else
+        {
+var pass = "notdetected"
+return pass
+        }
+    }
   }
   
-  var sutils = new Sutils ();
+  var $sutils = new Sutils ();
